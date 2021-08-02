7c7c4ebe67bdc30faefc47f4730f93347bbd8dd8
Elgar Weijtmans <elgar@weijtmans.org>
2021-08-02 16:49:46 +0200

    Working with popup

--- src/lambda/revolut.js
+++ src/lambda/revolut.js
@@ -2,23 +2,29 @@ import Axios from "axios"
 
 export async function handler(event, context) {
 
+  const data = JSON.parse(event.body)
+  console.log(data)
+
   const config = {
     headers: { Authorization: "Bearer " + process.env.REVOLUT_KEY }
   }
 
-  const bodyParameters = {
-      amount: 1,
-      currency: "USD"
+  const body = {
+      amount: data.amount,
+      currency: data.currency,
+      capture_mode: data.capture_mode,
+      description: data.description,
   }
 
   try {
     const response = await Axios.post( 
         'https://sandbox-merchant.revolut.com/api/1.0/orders',
-        bodyParameters,
+        body,
         config
     )
     const data = response.data
     // Success
+    console.log(data)
     return {
       statusCode: 200,
       body: JSON.stringify(data)
@@ -26,27 +32,17 @@ export async function handler(event, context) {
   } catch (error) {
       // Error
       if (error.response) {
-          /*
-          * The request was made and the server responded with a
-          * status code that falls out of the range of 2xx
-          */
           console.log(error.response.data);
           console.log(error.response.status);
           console.log(error.response.headers);
       } else if (error.request) {
-          /*
-          * The request was made but no response was received, `error.request`
-          * is an instance of XMLHttpRequest in the browser and an instance
-          * of http.ClientRequest in Node.js
-          */
           console.log(error.request);
       } else {
-          // Something happened in setting up the request and triggered an Error
           console.log('Error', error.message);
       }
       return {
         statusCode: 500,
-        body: JSON.stringify(error.message) // Could be a custom message or object i.e. JSON.stringify(err)
+        body: JSON.stringify(error.message)
       }
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,43 +1,57 @@
 import React, { Component } from "react"
+import RevolutCheckout from '@revolut/checkout'
+import Axios from 'axios'
 import "../styling/style.css"
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: JSON.stringify(json) }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("revolut")}>{loading ? "Loading..." : "Call Revolut API"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
-
 class Home extends Component {
+
+  callRevolut = () => async e => {
+    e.preventDefault()
+  
+    const body = {
+        amount: 0,
+        currency: "USD",
+        capture_mode: "MANUAL",
+        description: "Tykr Subsciption",
+    }
+
+      const response = await Axios.post( 
+          '/.netlify/functions/revolut',
+          body,
+          {}
+      )
+      const data = response.data
+      // Success
+      console.log(data)
+
+      const RC = await RevolutCheckout(data.public_id, 'sandbox')
+
+      RC.payWithPopup({
+        savePaymentMethodFor: "merchant",
+        name: "Elgar Weijtmans",
+        email: "elgar@weijtmans.org",
+        // Callback called when payment finished successfully
+        onSuccess() {
+          window.alert("Thank you!");
+        },
+        // Callback in case some error happened
+        onError(message) {
+          console.log(message)
+          window.alert("Oh no :(");
+        },
+        // (optional) Callback in case user cancelled a transaction
+        onCancel() {
+          window.alert("Payment cancelled!");
+        },
+      })
+
+  }
+
   render() {
     return (
       <div className="app">
         <header className="app-header">
-          <p>
-            Checkout
-          </p>
-          <LambdaDemo />
+          <button onClick={this.callRevolut()}>Call Revolut API</button>
         </header>
       </div>
     )
